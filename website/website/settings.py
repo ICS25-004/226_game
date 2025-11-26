b9bf0032bfbd98eb19d65849358a78b7f0d9e62c
ICS25-004 <ICS25-004@camosun.ca>
2025-11-26 15:38:03 -0800

    Define valid hosts in allowed hosts

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'tyK^.WN5RQc]64p9;Y4~Xz1/]n:vU([.$0zyAYVo8:^#[JmOf6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 RENDER_EXTERNAL_HOSTNAME = environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
